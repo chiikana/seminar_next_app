4b4f04fa968ab30126eb2e6d564c8b25b3806cf6
chiikana <pngnka710.pc@gmail.com>
2022-12-18 19:52:28 +0900

    init

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <ChakraProvider>
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
+  )
 }
+export default MyApp
