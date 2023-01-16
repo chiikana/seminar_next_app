fae04956f5e3f0d6c7b9efc2170d991597903c65
chiikana <pngnka710.pc@gmail.com>
2023-01-16 13:46:51 +0900

    signup and insert dousuru

--- web/components/MultiStepSignUp.jsx
+++ web/components/MultiStepSignUp.jsx
@@ -276,24 +276,24 @@ const Multistep = (props) => {
 
   const handleSignUp = async (e) => {
     e.preventDefault()
+    const { firstName, lastName, course, department, studentId } = data
 
     try {
-      const { data, error } = await supabase.auth.signUp(
+      const { user, ssession, error } = await supabase.auth.signUp(
         {
           email: fieldValues.email,
           password: fieldValues.pass,
+        },
+        {
+          data: {
+            firstName: fieldValues.firstName,
+            lastName: fieldValues.lastName,
+            // dateOfBirth: fieldValues.dateOfBirth,
+            course: fieldValues.course,
+            department: fieldValues.department,
+            studentId: fieldValues.studentId,
+          },
         }
-        // },
-        // {
-        //   data: {
-        //     firstName: fieldValues.firstName,
-        //     lastName: fieldValues.lastName,
-        //     // dateOfBirth: fieldValues.dateOfBirth,
-        //     course: fieldValues.course,
-        //     department: fieldValues.department,
-        //     studentId: fieldValues.firstName,
-        //   },
-        // }
       )
       console.log(data)
       if (error) throw error
@@ -302,36 +302,38 @@ const Multistep = (props) => {
       alert(error.error_description || error.message)
     }
 
-    try {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: fieldValues.email,
-        password: fieldValues.pass,
-      })
-      console.log(data)
-      if (error) throw error
-      alert("success for signin!")
-    } catch (error) {
-      alert(error.error_description || error.message)
-    }
+    // try {
+    //   const { data, error } = await supabase.auth.signInWithPassword({
+    //     email: fieldValues.email,
+    //     password: fieldValues.pass,
+    //   })
+    //   console.log(data)
+    //   if (error) throw error
+    //   alert("success for signin!")
+    // } catch (error) {
+    //   alert(error.error_description || error.message)
+    // }
 
-    try {
-      const { data, error } = await supabase.from("profiles").insert(
-        [
-          { lastName: fieldValues.lastName },
-          { firstName: fieldValues.firstName },
-          // {dateOfBirth: fieldValues.dateOfBirth},
-          { course: fieldValues.course },
-          { department: fieldValues.department },
-          { studentId: fieldValues.firstName },
-        ],
-        { upsert: true }
-      )
-      console.log(data)
-      if (error) throw error
-      alert("success for insert!")
-    } catch (error) {
-      alert(error.error_description || error.message)
-    }
+    // try {
+    //   const { data, error } = await supabase
+    //     .from("profiles")
+    //     .insert(
+    //       [
+    //         { lastName: fieldValues.lastName },
+    //         { firstName: fieldValues.firstName },
+    //         { dateOfBirth: fieldValues.dateOfBirth },
+    //         { course: fieldValues.course },
+    //         { department: fieldValues.department },
+    //         { studentId: fieldValues.firstName },
+    //       ],
+    //       { upsert: true }
+    //     )
+    //   console.log(data)
+    //   if (error) throw error
+    //   alert("success for insert!")
+    // } catch (error) {
+    //   alert(error.error_description || error.message)
+    // }
   }
 
   const handleSignOut = async (e) => {
