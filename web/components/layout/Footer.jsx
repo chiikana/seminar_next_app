603610c23d3ae4f52388c59c06e871ca2e413c86
chiikana <pngnka710.pc@gmail.com>
2023-01-26 11:13:43 +0900

    footer小さくした

--- web/components/layout/Footer.jsx
+++ web/components/layout/Footer.jsx
@@ -58,78 +58,9 @@ export const Footer = () => {
           borderTopWidth={1}
           borderStyle={"solid"}
           borderColor={useColorModeValue("gray.200", "gray.700")}
-        >
-          <Container
-            as={Stack}
-            maxW={"6xl"}
-            py={4}
-            direction={{ base: "column", md: "row" }}
-            spacing={4}
-            justify={{ base: "center", md: "space-between" }}
-            align={{ base: "center", md: "center" }}
-          >
-            <Text>© kanato suzaki. All rights reserved</Text>
-            <Stack direction={"row"} spacing={6}>
-              <SocialButton
-                label={"Twitter"}
-                onClick={() => {
-                  router.push("/JumpPage/")
-                }}
-                // href={"/JumpPage/"}
-              >
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton
-                label={"YouTube"}
-                onClick={() => {
-                  router.push("/JumpPage/")
-                }}
-              >
-                <FaYoutube />
-              </SocialButton>
-              <SocialButton
-                label={"Instagram"}
-                onClick={() => {
-                  router.push("/JumpPage/")
-                }}
-              >
-                <FaInstagram />
-              </SocialButton>
-            </Stack>
-          </Container>
-        </Box>
+        ></Box>
       </Box>
       {/* <Box as="p">フッター下だよ</Box> */}
     </>
-  )
-}
-
-const SocialButton = ({
-  children,
-  label,
-  // href,
-  onClick,
-}) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-      rounded={"full"}
-      w={8}
-      h={8}
-      cursor={"pointer"}
-      as={"a"}
-      // href={href}
-      onClick={onClick}
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{
-        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
   )
 }
